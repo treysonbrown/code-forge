abfadae5f9d80a87af6253b1355990ec1d6ad231
treslecheson <tr3ysonb@gmail.com>
2025-07-10 09:46:42 -0600

    Header Sizing

--- code-forge-frontend/src/components/Header.tsx
+++ code-forge-frontend/src/components/Header.tsx
@@ -4,13 +4,15 @@ import WhiteHeader from "./WhiteHeader";
 type HeaderProps = {
 	whiteText: string;
 	blueText: string;
+	textSize: number;
+
 }
 
-const Header: React.FC<HeaderProps> = ({ whiteText, blueText }) => {
+const Header: React.FC<HeaderProps> = ({ whiteText, blueText, textSize }) => {
 	return (
 		<div className="flex justify-center gap-3">
-			<WhiteHeader text={whiteText} />
-			<BlueHeader text={blueText} />
+			<WhiteHeader text={whiteText} textSize={textSize} />
+			<BlueHeader text={blueText} textSize={textSize} />
 		</div>
 	)
 }
--- code-forge-frontend/src/pages/PersonalStats.tsx
+++ code-forge-frontend/src/pages/PersonalStats.tsx
@@ -1,9 +1,16 @@
 import Header from "../components/Header";
+import WhiteHeader from "../components/WhiteHeader";
 
 const PersonalStats = () => {
 	return (
 		<div>
-			<Header whiteText="PERSONAL" blueText="STATS" />
+			<Header whiteText="PERSONAL" blueText="STATS" textSize={6} />
+			<div className="flex flex-col justify-center">
+				<WhiteHeader text="DAILY STREAK" textSize={4} />
+				<WhiteHeader text="DAYS" textSize={4} />
+				<WhiteHeader text="POINTS" textSize={4} />
+				<Header whiteText="QUESTIONS" blueText="ANSWERED" textSize={4} />
+			</div>
 		</div>
 	)
 }
