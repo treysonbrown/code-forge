5b46db577ce50f03804a1772416c2ce0fab235e2
treslecheson <tr3ysonb@gmail.com>
2025-07-17 12:02:49 -0600

    Remove image

--- code-forge-frontend/src/components/Login.tsx
+++ code-forge-frontend/src/components/Login.tsx
@@ -3,9 +3,6 @@ import LoginCard from "./LoginCard";
 const Login = () => {
 	return (
 		<div className="grid grid-cols-2">
-			<div className="mt-20">
-				<img src="../../src/assets/Login.png" width="" />
-			</div>
 			<div className="flex justify-center mt-40">
 				<LoginCard />
 			</div >
